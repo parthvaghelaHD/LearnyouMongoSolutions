58253d9da50b340d694b2b1e73378a7e0cef1319
parthvaghelahd <parthvaghelahd@gmail.com>
2020-01-22 18:12:41 +0530

    find({ ... code ... }) operations done in mongodb

---
+++ solutions.js
@@ -0,0 +1,15 @@
+const mongo = require('mongodb').MongoClient;
+const process = require('process');
+const url = 'mongodb://localhost:27017/learnyoumongo'
+const age = parseInt(process.argv[2])
+
+mongo.connect(url, function(err, db) {
+      // db gives access to the database
+	let data = db.db('learnyoumongo');
+	data.collection('parrots').find({ age : {$gt: age}}).toArray(function(err, documents){
+	if (err) throw err;
+	console.log(documents)
+	data.close();
+});
+});
+
