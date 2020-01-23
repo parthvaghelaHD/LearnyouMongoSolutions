117b2127b832669506d400b54c8dc47912508094
parthvaghelahd <parthvaghelahd@gmail.com>
2020-01-23 10:11:27 +0530

    insert query in mongodb

--- solutions.js
+++ solutions.js
@@ -1,22 +1,36 @@
 const mongo = require('mongodb').MongoClient;
 const process = require('process');
 const url = 'mongodb://localhost:27017/learnyoumongo'
-const age = parseInt(process.argv[2])
+const firstName = process.argv[2];
+const lastName = process.argv[3];
 
 mongo.connect(url, function(err, db) {
-      // db gives access to the database
+			// find query in Mongodb
+			
 /*	let data = db.db('learnyoumongo');
 	data.collection('parrots').find({ age : {$gt: age}}).toArray(function(err, documents){
 	if (err) throw err;
 	console.log(documents)
 	data.close();
 */
-	let data = db.db('learnyoumongo');
-        data.collection('parrots').find({ age : {$gt: +age}},{projection: {name : 1,age:1,_id:0}}).toArray(function(err, documents){
-        if (err) throw err;
-        console.log(documents)
-        data.close();
 
-});
+		// find Project in mongodb
+	/* let data = db.db('learnyoumongo');
+      //   data.collection('parrots').find({ age : {$gt: +age}},{projection: {name : 1,age:1,_id:0}}).toArray(function(err, documents){
+      //   if (err) throw err;
+      //   console.log(documents)
+      data.close();
+*/
+		// insert in mongodb
+			let data = db.db("learnyoumongo");
+			const obj = {
+				firstName: firstName,
+				lastName: lastName
+			}
+			data.collection('docs').insert(obj, function (err, data) {
+				if (err) throw err;
+				console.log(JSON.stringify(obj));
+			})
+			db.close();
 });
 
