954019883d04d25c9515d5e66d314954222ae166
parthvaghelahd <parthvaghelahd@gmail.com>
2020-01-23 09:38:17 +0530

    find project completed

--- solutions.js
+++ solutions.js
@@ -5,11 +5,18 @@ const age = parseInt(process.argv[2])
 
 mongo.connect(url, function(err, db) {
       // db gives access to the database
-	let data = db.db('learnyoumongo');
+/*	let data = db.db('learnyoumongo');
 	data.collection('parrots').find({ age : {$gt: age}}).toArray(function(err, documents){
 	if (err) throw err;
 	console.log(documents)
 	data.close();
+*/
+	let data = db.db('learnyoumongo');
+        data.collection('parrots').find({ age : {$gt: +age}},{projection: {name : 1,age:1,_id:0}}).toArray(function(err, documents){
+        if (err) throw err;
+        console.log(documents)
+        data.close();
+
 });
 });
 
